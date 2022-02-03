4fd6644798a6610c17ce3618beaa8d2f56dc18e2
Sathyanarayanan <svvsathyanarayanan@gmail.com>
2022-02-03 10:44:55 -0800

    fix : add 404, projects and proper redirection

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,40 @@
-function init() {
-  document.getElementById('send-message').onclick = function (ev) {
-    console.log(document.getElementById('get-in-touch').valid);
+function submit(ev) {
+  if (isValid()) {
     const fullName = document.getElementById('fullname').value;
     const email = document.getElementById('emailaddress').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('comments').value;
-    console.log({
-      fullName,
-      email,
-      subject,
-      message,
-    });
-  };
+    axios
+      .post('api/contact-us', { fullName, email, subject, message })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    onBlur();
+  }
+}
+
+function isValid() {
+  const fullName = document.getElementById('fullname').value;
+  const email = document.getElementById('emailaddress').value;
+  const subject = document.getElementById('subject').value;
+  const validEmail = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  ).test(email);
+
+  return fullName && email && subject && validEmail;
+}
+
+function onBlur() {
+  document.getElementById('send-message').disabled = !isValid();
+}
+function init() {
+  document.getElementById('send-message').onclick = submit;
+  document.getElementById('send-message').disabled = isValid;
+  document.getElementById('fullname').onblur = onBlur;
+  document.getElementById('emailaddress').onblur = onBlur;
+  document.getElementById('subject').onblur = onBlur;
+  document.getElementById('comments').onblur = onBlur;
 }
 
 document.addEventListener('readystatechange', function () {
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,5 @@
 import { AuthGuard } from '@nestjs/passport';
-import {
-  Controller,
-  Get,
-  HttpStatus,
-  Render,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -18,7 +11,7 @@ export class AppController {
         description:
           'A basic blogging web app which is built on top of <b>Angular 8 and Firebase</b>. The user can view, create, edit and delete their own blogs. User can book tickets for tours and provide them offline to validate the purchase. <br/> Logo Courtesy: Sakthibala',
         link: 'https://vibrant-nightingale-1f752e.netlify.app/',
-        img: '/img/social.jpeg',
+        img: 'https://vibrant-nightingale-1f752e.netlify.app/assets/images/finalized.jpg',
         class: "class='btn btn-warning rounded-pill'",
         social: true,
       },
@@ -27,7 +20,7 @@ export class AppController {
         description:
           'An E-commerce application used to rent products, hire people and opt services for events like Engagement. The application is built on <b>Next.JS, Nest.JS, TypeORM and MySQL</b>. Deployments are done using <b>Heroku and Netlify</b> which involves CI/CD',
         link: 'https://www.lotusdecoreandrental.com',
-        img: '/img/lotus.png',
+        img: 'https://lotusdecoreandrental.com/logo.png',
         class: "class='btn btn-pink rounded-pill'",
         lotus: true,
       },
@@ -36,13 +29,12 @@ export class AppController {
         description:
           "This project is used for managing compliance of companies incorporated. This project is being used by 25 users and it's currently in testing phase. This project involved <b>Vue.JS, Nest.JS, MongoDB, Celery, Redis Queue and Docker Container </b>",
         link: 'https://sathyanarayanan-v.github.io/AmplifyCClient/',
-        img: '/img/amplifyc.png',
+        img: 'https://sathyanarayanan-v.github.io/AmplifyCClient/img/logo.3d45e3b5.svg',
         class: "class='btn btn-primary rounded-pill'",
         amplifyc: true,
       },
     ];
   }
-  // @UseGuards(AuthGuard('jwt'))
   @Get('')
   @Render('home')
   getStatus() {
@@ -65,5 +57,10 @@ export class AppController {
   @Render('home')
   home() {
     return { message: 'hello' };
+  }
+
+  @Post('api/contact-us')
+  sendMessage(@Body() mail) {
+    console.log(mail);
   }
 }
